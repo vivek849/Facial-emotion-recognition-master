1d0100da670c31d865f42e9c48500ed1a8c219dc
Vivek Kumar Chaurasiya <90383948+vivek849@users.noreply.github.com>
2025-01-06 23:08:10 +0530

    img_predict.py

--- img_predict.py
+++ img_predict.py
@@ -2,7 +2,7 @@ import cv2
 import argparse
 import numpy as np
 from keras.models import model_from_json
-from keras.preprocessing import image 
+from tensorflow.keras.preprocessing import image 
 
 # Parse the arguments
 ap = argparse.ArgumentParser()
